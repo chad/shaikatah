04acdb9cbe88ca437e71759baf001f6a452c6012
Chad Fowler <chad@chadfowler.com>
2009-10-16 11:55:53 -0300

    some refactoring

--- lib/application.rb
+++ lib/application.rb
@@ -5,6 +5,7 @@ class Application
   include HotCocoa
   
   class MyView < NSView
+    attr_reader :slides
     def acceptsFirstResponder
       true
     end
@@ -12,28 +13,84 @@ class Application
     def acceptsFirstMouse(event)
       true
     end    
-
     
+    def words=(words)
+      self.slides = words.map do |word|
+        text_layer_with(word)
+      end
+    end
+    
+    def slides=(slides)
+      @slides = slides
+      @current_slide = 0
+      @slides.first.opacity = 1.0
+      @slides.first.setHidden false
+    end
+
     def keyDown(event)
       characters = event.characters      
       if characters.length == 1 && !event.isARepeat
         character = characters.characterAtIndex(0)
         if character == NSLeftArrowFunctionKey
-          @on_previous_slide.call
+          on_previous_slide
         else
-          @on_next_slide.call
+          on_next_slide
         end
       end
     end
 
-    def on_next_slide(&block)
-      @on_next_slide = block
-    end
+    private
     
-    def on_previous_slide(&block)
-      @on_previous_slide = block
-    end
+      def on_next_slide
+        NSAnimationContext.currentContext.setDuration 3.0
+        slide = @slides[@current_slide]
+        @current_slide += 1
+        factor = rand(2) > 0 ? -5.0 : 10.0
+        transform = CATransform3DMakeScale(factor, factor, factor)
+        slide.setTransform transform
+        slide.opacity = 0.0
+        exit if @slides[@current_slide].nil?
+        @slides[@current_slide].opacity = 1.0
+      end
     
+      #FIXME: broken - need to reset position
+      def on_previous_slide
+        return if @current_slide == 0
+        slide = @slides[@current_slide]
+        slide.opacity = 0.0
+        @current_slide -= 1
+        @slides[@current_slide].position = text_starting_position
+        @slides[@current_slide].opacity = 1.0
+      end
+
+      def random_color_code
+        rand(100.0) * 0.01
+      end
+
+      def text_layer_with(word)
+        screen_size = NSScreen.mainScreen.frame.size
+        headerTextLayer = CATextLayer.layer
+        headerTextLayer.name = "header"
+        headerTextLayer.string = word
+        headerTextLayer.foregroundColor = CGColorCreateGenericRGB(random_color_code, random_color_code, random_color_code, 1.0)
+        headerTextLayer.style = {"font" => "BankGothic-Light","alignmentMode" => "center"}
+        headerTextLayer.fontSize = 104
+        headerTextLayer.wrapped = true
+        headerTextLayer.opacity = 0.0
+        headerTextLayer.frame = frame
+        headerTextLayer.contentsGravity = "center"
+        headerTextLayer.position = text_starting_position
+        layer.addSublayer headerTextLayer
+        headerTextLayer
+      end
+      
+      def screen_size
+        @screen_size ||= NSScreen.mainScreen.frame.size
+      end
+      
+      def text_starting_position
+        @text_starting_position ||= [(screen_size.width)/2, 100]
+      end
   end
 
   attr_reader :words, :win
@@ -47,46 +104,8 @@ class Application
       app.delegate = self
       window frame: NSScreen.mainScreen.frame, title: "Shaikatah", styleMask:NSBorderlessWindowMask, defer:false, view: :nolayout do |win|
         @win = win
-        def win.canBecomeMainWindow
-          true
-        end
-        def win.canBecomeKeyWindow
-          true
-        end
+        enable_main_and_key!
         setup_view!
-        
-        slides = words.map do |word|
-          text_layer_with(word)
-        end
-        
-        slides.first.opacity = 1.0
-        slides.first.setHidden false
-        current_slide = 0
-        win.view.layer.backgroundColor = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1.0)
-
-        win.view.on_next_slide do 
-          NSAnimationContext.currentContext.setDuration 3.0
-          slide = slides[current_slide]
-          # slide.opacity = 0.0
-          current_slide += 1
-          factor = rand(2) > 0 ? -5.0 : 10.0
-          transform = CATransform3DMakeScale(factor, factor, factor)
-          slide.setTransform transform
-          slide.opacity = 0.0
-          
-          exit if slides[current_slide].nil?
-          slides[current_slide].opacity = 1.0
-        end
-        
-        win.view.on_previous_slide do
-          next if current_slide == 0
-          
-          slide = slides[current_slide]
-          slide.opacity = 0.0
-          current_slide -= 1
-          slides[current_slide].opacity = 1.0
-        end
-        
         win.will_close { exit }
         win.level = CGShieldingWindowLevel()
         win.makeKeyAndOrderFront(nil)
@@ -100,30 +119,18 @@ class Application
       win.view = MyView.new
       win.view.wantsLayer = true
       win.setInitialFirstResponder(win.view)
+      win.view.words = words
+      win.view.layer.backgroundColor = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1.0)
     end
-  
-    def random_color_code
-      rand(100.0) * 0.01
+
+    def enable_main_and_key!
+      def win.canBecomeMainWindow
+        true
+      end
+      def win.canBecomeKeyWindow
+        true
+      end
     end
-  
-    def text_layer_with(word)
-      screen_size = NSScreen.mainScreen.frame.size
-      headerTextLayer = CATextLayer.layer
-      headerTextLayer.name = "header"
-      headerTextLayer.string = word
-      headerTextLayer.foregroundColor = CGColorCreateGenericRGB(random_color_code, random_color_code, random_color_code, 1.0)
-      headerTextLayer.style = {"font" => "BankGothic-Light","alignmentMode" => "center"}
-      headerTextLayer.fontSize = 104
-      headerTextLayer.wrapped = true
-      headerTextLayer.opacity = 0.0
-      headerTextLayer.frame = win.view.frame#????
-      headerTextLayer.contentsGravity = "center"
-      screen_size = NSScreen.mainScreen.frame.size
-      headerTextLayer.position = [(screen_size.width)/2, 100]
-      win.view.layer.addSublayer headerTextLayer
-      headerTextLayer
-    end
-  
 end
 
 def Takahashi!(*words)
